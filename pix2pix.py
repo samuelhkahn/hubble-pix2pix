b643318ec5ed5bb9ee90622b13d801d09478d2c5
Samuel Kahn <sam@tickr.com>
2022-02-08 19:03:24 -0700

    whoops

--- pix2pix.py
+++ pix2pix.py
@@ -94,7 +94,7 @@ class Pix2Pix:
 
         scattering_loss = (scat_real - scat_fake).abs().sum(axis=(1, 2, 3)).mean()
 
-        total_loss = self.lr*(adversarial_loss\
+        total_loss = self.lr*(self.lambda_adv*adversarial_loss\
                      + self.lambda_recon*recon_loss\
                      + self.lambda_vgg*vgg_loss\
                      + self.lambda_scattering*scattering_loss)
