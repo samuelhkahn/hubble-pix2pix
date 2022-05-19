3ae0a97663bc2a3bb9b32fe650e2a024983f1d88
Samuel Kahn <sam@tickr.com>
2022-05-19 06:43:23 -0700

    cleaning code

--- batchwise_gaussian_noise.py
+++ batchwise_gaussian_noise.py
@@ -29,23 +29,15 @@ import torch
 #         return x
 
 class BatchWiseGaussianNoise(nn.Module):
-    """Gaussian noise regularizer.
+#     """Gaussian Wise Noise .
 
-    Args:
-        sigma (float, optional): relative standard deviation used to generate the
-            noise. Relative means that it will be multiplied by the magnitude of
-            the value your are adding the noise to. This means that sigma can be
-            the same regardless of the scale of the vector.
-        is_relative_detach (bool, optional): whether to detach the variable before
-            computing the scale of the noise. If `False` then the scale of the noise
-            won't be seen as a constant but something to optimize: this will bias the
-            network to generate vectors with smaller values.
-    """
+#     Args:
+#         zero_mean (bool, optional): whether to generate the noise centered @ zero 
+#     """
 
     def __init__(self,zero_mean = True,  is_relative_detach=True):
         super().__init__()
         self.zero_mean = zero_mean
-        self.is_relative_detach = is_relative_detach
         self.register_buffer('noise', torch.tensor(0.0))
 
     def forward(self, x):
